f59ac8af32c6b36fd41426cf1e9ea9762efe7788
DylanToporek <dylantoporek@gmail.com>
2021-09-22 16:25:55 -0400

    commit tree ef2fb0394528525ab10c4e0d6f629f44e63afb16

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+customerName = 'bob';  
+    customerName;
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+  
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFav = 'jim';
+    leastFav = 6;
+ }
+ 
+ changeLeastFavoriteCustomer();
